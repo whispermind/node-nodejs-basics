97dea6b3ec021479c1449526611a4c78954f00ae
whispermind <h4vefun.dieyoung@gmail.com>
2024-10-07 21:32:25 +0300

    feat/implement zip-module solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { pipeline } from "stream";
+import { ReadStream, WriteStream } from "fs";
+import { join } from "path";
+import { createGzip } from "zlib";
+
+import { getPaths } from "../helpers/index.js";
+
+const { __dirname } = getPaths(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, "files");
+    const source = join(folderPath, "fileToCompress.txt");
+    const destination = join(folderPath, "archive.gz");
+
+    const gzip = createGzip();
+
+    pipeline(ReadStream(source), gzip, WriteStream(destination), (e) => {
+        if(e) {
+            console.log(e);
+        }
+    })
 };
 
 await compress();
